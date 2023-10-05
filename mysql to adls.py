b1fd1e72458111912b049fd7b28a50adf0338e0b
dm-databricks-gitrepo <dm-databricks-gitrepo>
2023-10-05 10:07:19 +0000

    added changes

--- mysql to adls.py
+++ mysql to adls.py
@@ -10,3 +10,34 @@ mysql_secret_name = "mysql-db"
 adls_secret_name="adls-accesskey"
 
 
+
+# COMMAND ----------
+
+# Retrieve MySQL credentials from Azure Key Vault using the provided scope_name and mysql_secret_name
+secret = dbutils.secrets.get(scope_name, mysql_secret_name)
+
+# Parse the retrieved secret as JSON to access the individual credentials
+mssql_cred = json.loads(secret)
+
+host=mssql_cred["host"]
+port=mssql_cred["port"]
+user=mssql_cred["user"]
+password=mssql_cred["password"]
+
+
+# COMMAND ----------
+
+# Retrieving data from a MySQL table
+
+
+mysql_table_df = (spark.read
+  .format("mysql")
+  .option("dbtable", 'employees')
+  .option("host", host)
+  .option("port", port)
+  .option("database", 'my_db')
+  .option("user",user)
+  .option("password", password)
+  .load()
+)
+display(mysql_table_df)
